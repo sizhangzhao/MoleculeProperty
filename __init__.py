35f275520929907a74248d9c7335c16b4c1555e6
sizhangzhao <sz.zhao@foxmail.com>
2019-07-14 20:08:09 -0400

    Delete __init__.py

--- __init__.py
+++
@@ -1,5 +0,0 @@
-from .DataExtractor import *
-from .MoleculeGraphDataset import *
-from .MoleculeGraphDataloader import MoleculeGraphDataLoader
-from .MoleculeMPNN import MoleculeMPNN
-from .MoleculeGNNTrainer import *
